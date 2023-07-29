955cda38ab22e0976a75b9cfe20dadabda7f5822
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-07-29 23:51:32 +0530

    add dbconnection

---
+++ src/main/java/com/example/Challenge1/db/DBConnection.java
@@ -0,0 +1,22 @@
+package com.example.Challenge1.db;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static Connection con;
+    public static Connection getConnection(){
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/item");
+            con = pool.getConnection();
+
+        } catch (NamingException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
+}
